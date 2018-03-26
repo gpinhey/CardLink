b3faf698d68d369bdabcaeeb5126cc02606ce95c
Graham Pinhey <gpinhey@uwaterloo.ca>
2018-03-25 18:19:00 -0600

    Preventing cards from overlapping, brute-force right now

--- src/reducers.js
+++ src/reducers.js
@@ -1,13 +1,22 @@
 ﻿import { combineReducers } from 'redux';
 
+function cardExists(cards, place) {
+  if (cards.find(card => (card.x === place.x && card.y === place.y))) {
+    return true;
+  }
+  return false;
+}
+
 function cards(state=[], action) {
   switch(action.type) {
     case "ADD_CARD":
+      if (cardExists(state, action)) return state;
       const newCard = [{id: action.id,
                         x: action.x,
                         y: action.y}];
       return state.concat(newCard);
     case "MOVE_CARD":
+      if (cardExists(state, action)) return state;
       return state.map((card) => {
         if (card.id === action.id) {
           return Object.assign({}, card, {
